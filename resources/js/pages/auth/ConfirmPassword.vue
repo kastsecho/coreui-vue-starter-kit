7d0b0ef78c9b0fa5bc7d0989f25d2070270e8843
Jared Lewis <17649602+jrd-lewis@users.noreply.github.com>
2025-03-21 19:33:54 -0400

    Update ConfirmPassword.vue

--- resources/js/pages/auth/ConfirmPassword.vue
+++ resources/js/pages/auth/ConfirmPassword.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-// import InputError from '@/components/InputError.vue';
+import InputError from '@/components/InputError.vue';
 import AuthLayout from '@/layouts/AuthLayout.vue';
+import { CButton, CFormInput, CFormLabel, CSpinner } from '@coreui/vue';
 import { Head, useForm } from '@inertiajs/vue3';
 
 const form = useForm({
@@ -17,5 +18,29 @@ const submit = () => {
 <template>
     <AuthLayout title="Confirm your password" description="This is a secure area of the application. Please confirm your password before continuing.">
         <Head title="Confirm password" />
+
+        <form @submit.prevent="submit">
+            <div class="d-grid gap-3">
+                <div class="d-grid">
+                    <CFormLabel for="password">Password</CFormLabel>
+                    <CFormInput
+                        id="password"
+                        type="password"
+                        v-model="form.password"
+                        required
+                        autocomplete="current-password"
+                        autofocus
+                        :invalid="!!form.errors.password"
+                    />
+
+                    <InputError :message="form.errors.password" />
+                </div>
+
+                <CButton type="submit" color="primary" :disabled="form.processing">
+                    <CSpinner v-if="form.processing" size="sm" />
+                    Confirm Password
+                </CButton>
+            </div>
+        </form>
     </AuthLayout>
 </template>
